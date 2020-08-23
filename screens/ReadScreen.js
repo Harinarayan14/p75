1cde9bb401f145b0f0beb21de8b1ea1f26097ca7
harinarayan14 <gjayashankar2008@gmail.com>
2020-08-23 20:46:38 +0530

    P73

--- screens/ReadScreen.js
+++ screens/ReadScreen.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-export default class ReadScreen extends React.Component {
-render(){
-    return(
+import { Text, View ,FlatList} from 'react-native';
+import db from '../config';
+
+export default class Readscreen extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      stories:[],
+      lastDocument:null
+    }
+  }
+
+  componentDidMount= async()=>{
+    const Story = await db.collection("Stories").limit(10).get();
+    Story.docs.map((doc)=>{
+       this.setState({
+         stories:[...this.state.stories,doc.data()],
+         lastDocument:doc
+       })
+
+    })
+
+  }
+
+  collectData = async()=>{
+    const Stories = await db.collection("Stories").startAfter(this.state.lastDocument)
+    .limit(10).get();
+    Stories.docs.map((doc)=>{
+      this.setState({
+        stories:[...this.state.stories,doc.data()],
+        lastDocument:doc
+      })
+    })
+  }
+    render() {
+      return (
         <View>
-            <Text>
-                Read Stories
-            </Text>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Search</Text >
         </View>
-    )}
-}
+        <FlatList data = {this.state.Alltransactions} 
+        renderItem={({item})=>{
+        <View style={{borderWidth:3}} >
+              <Text>{"Title:" + item.Title}</Text>
+              <Text>{"Author:" + item.AuthorName}</Text>
+        </View>
+        }
+      } 
+      keyExtractor={(item,index)=>{
+        index.toString();
+      }} 
+      onEndReached={this.collectData}
+      onEndReachedThreshold={0.8}
+      />
+          
+          
+        
+        </View>
+      );
+    }
+  }     
